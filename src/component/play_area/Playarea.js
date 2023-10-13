5387b774f34499f71b7e848bbcd8ec83b2c123ed
AnujMking25 <anuj170499@gmail.com>
2023-10-13 16:06:32 +0530

    now one by one mole are seeinf outside of hole

--- src/component/play_area/Playarea.js
+++ src/component/play_area/Playarea.js
@@ -1,27 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classess from './Play_area.module.css' 
 import Mallet from '../mallet/Mallet'
 import Mole from './mole/Mole'
 const Play_area = () => {
-  const [showMole,setshowMole]=useState(false);
+  const [showMole,setshowMole]=useState(null);
+  const [same,setsame]=useState(0);
     function rondomTime(min,max){
         return Math.round(Math.random()*(max-min)+min)
       }
+
+  if(same===showMole){
+    setsame( Math.floor(Math.random()*5))
+  }
+
+
+     useEffect(()=>{
       setTimeout(()=>{
-        setshowMole(!showMole)
+        setshowMole(same)
       },rondomTime(200,2000))
+     },[same])
     
   return (
     <div className={classess.maindiv}>
       <div className={classess.playarea}>
       <div className={classess.hole}>
-         <div className={classess.hole_top}><Mole/></div>
-         <div className={classess.hole_top}><Mole/></div>
+         <div className={classess.hole_top}>{(showMole===0) ? <Mole/>:null}</div>
+         <div className={classess.hole_top}>{(showMole===1) ? <Mole/>:null}</div>
       </div>
       <div className={classess.hole}>
-        <div className={classess.hole_bottom}><Mole/></div>
-        <div className={classess.hole_bottom}><Mole/></div>
-        <div className={classess.hole_bottom}><Mole/></div>
+        <div className={classess.hole_bottom}>{(showMole===2) ? <Mole/>:null}</div>
+        <div className={classess.hole_bottom}>{(showMole===3) ? <Mole/>:null}</div>
+        <div className={classess.hole_bottom}>{(showMole===4) ? <Mole/>:null}</div>
         </div>
       </div>
       <Mallet/>
